712b43e62f2f31a4ffa8db558cc1e62ae4d92152
avnerjose <avnerj.g.r@gmail.com>
2022-05-21 20:58:37 -0300

    Updatint tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      gridTemplateColumns: {
+        'custom': '1fr 350px',
+      },
+    }
   },
   plugins: [],
 }
